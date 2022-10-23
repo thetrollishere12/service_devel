4661451b1cf6c2239465836d63920b69352d9d82
Vatler Lett <business.xfree2000@gmail.com>
2022-10-22 19:16:04 -0700

    add time line

--- resources/js/CustomComponents/SelectHours.tsx
+++ resources/js/CustomComponents/SelectHours.tsx
@@ -1,22 +1,70 @@
-import React from 'react';
+import { floor } from 'lodash';
+import React, { useState } from 'react';
 
-export default function SelectHours(){
+interface IProp {
+	change: any,
+	term: any,
+	index: any,
+	sub: any,
+}
+
+export default function SelectHours(props: IProp) {
+	const interval = 15;
+	const count = 60 * 24 / 15;
+	const half = count / 2;
+	const step = 60 / interval;
+	const [from, setFrom] = useState(props.term.from);
+	const [to, setTo] = useState(props.term.to);
+
+	const arr = new Array<number>(count);
+	for (let i = 0; i < count; i++) {
+		arr[i] = i * interval;
+	}
+
+	const changeFrom = (e: any) => {
+		const { change, term, index, sub } = props;
+		if (parseInt(e.target.value) > parseInt(term.to)) {
+			// alert("wrong");
+			setFrom(term.to);
+		} else {
+			term.from = e.target.value;
+			change(index, sub, term);
+			setFrom(e.target.value);
+		}
+	}
+	const changeTo = (e: any) => {
+		const { change, term, index, sub } = props;
+		if (parseInt(e.target.value) < parseInt(term.from)) {
+			// alert("wrong");
+			setTo(to);
+		} else {
+			term.to = e.target.value;
+			change(index, sub, term);
+			setTo(e.target.value);
+		}
+	}
 
 	return (
-		<select className="text-xs rounded border-gray-200">
-	        <option>12:00 AM</option>
-	        <option>12:05 AM</option>
-	        <option>12:10 AM</option>
-	        <option>12:15 AM</option>
-	        <option>12:20 AM</option>
-	        <option>12:25 AM</option>
-	        <option>12:30 AM</option>
-	        <option>12:35 AM</option>
-	        <option>12:40 AM</option>
-	        <option>12:45 AM</option>
-	        <option>12:50 AM</option>
-	        <option>12:55 AM</option>
-	    </select>
+		<div style={{ display: "contents" }}>
+			<select onChange={changeFrom} className="text-xs rounded border-gray-200">
+				{
+
+					arr.map((data, i) =>
+						<option key={i} selected={parseInt(from) == i ? true : false} value={i} >{floor((i % half) / step) < 10 && 0}{floor((i % half) / step)} : {floor(i % half) % step * interval < 10 && 0}{floor(i % half) % step * interval} {i < arr.length / 2 ? "AM" : "PM"}</option>
+					)
+				}
+			</select>
+			<select onChange={changeTo} className="text-xs rounded border-gray-200">
+				{
+
+					arr.map((data, i) => i > parseInt(from) &&
+						<option key={i} selected={parseInt(to) == i ? true : false} value={i} >{floor((i % half) / step) < 10 && 0}{floor((i % half) / step)} : {floor(i % half) % step * interval < 10 && 0}{floor(i % half) % step * interval} {i < arr.length / 2 ? "AM" : "PM"}</option>
+					)
+				}
+			</select>
+			<br />
+		</div>
+
 	)
 
 }
--- resources/js/Pages/Profile/Business/Screener/4.tsx
+++ resources/js/Pages/Profile/Business/Screener/4.tsx
@@ -1,4 +1,4 @@
-import React,{ useState } from 'react';
+import React, { useState } from 'react';
 import AppLayout from '@/Layouts/AppLayout';
 import ScreenerStage from '@/CustomComponents/ScreenerStage';
 import Checkbox from '@/Components/Checkbox';
@@ -6,62 +6,92 @@ import SelectHours from '@/CustomComponents/SelectHours';
 
 import { InertiaLink } from '@inertiajs/inertia-react';
 
+type period = {
+  from: string;
+  to: string;
+}
+
 export default function Dashboard() {
 
-    // constructor(){
-    //     super(){
+  // constructor(){
+  //     super(){
 
-    //         this.state = {
-    //             "monday": [
-    //             {
-    //                 "from":0,
-    //                 "to":24
-    //             },{
-    //                 "from":0,
-    //                 "to":24
-    //             }],
-    //             "tuesday":[],
-    //             "wednesday":[],
-    //             "thursday":[],
-    //             "friday":[],
-    //             "saturday":[],
-    //             "sunday":[],
-    //         };
+  //         this.state = {
+  //             "monday": [
+  //             {
+  //                 "from":0,
+  //                 "to":24
+  //             },{
+  //                 "from":0,
+  //                 "to":24
+  //             }],
+  //             "tuesday":[],
+  //             "wednesday":[],
+  //             "thursday":[],
+  //             "friday":[],
+  //             "saturday":[],
+  //             "sunday":[],
+  //         };
 
-    //     }
-    // }
+  //     }
+  // }
 
 
-    const [date, setInputFields] = useState({
-        monday: [
-        {
-            from:0,
-            to:24
-        },{
-            from:0,
-            to:24
-        }],
-        tuesday:[],
-        wednesday:[],
-        thursday:[],
-        friday:[],
-        saturday:[],
-        sunday:[],
-    });
 
-    console.log(date);
+  // {
+  //   from: 0,
+  //     to: 24
+  // }
 
-    function add(day) {
-        setInputFields([...date, {
-            from:0,
-            to:24
-        }])
-        console.log(date);
+  const [date, setInputFields] = useState([
+    [{ from: 0, to: 95, status: "add" }],
+    [{ from: 0, to: 95, status: "add" }],
+    [{ from: 0, to: 95, status: "add" }],
+    [{ from: 0, to: 95, status: "add" }],
+    [{ from: 0, to: 95, status: "add" }],
+    [{ from: 0, to: 95, status: "add" }],
+    [{ from: 0, to: 95, status: "add" }],
+  ]);
+  const [checkState, setCheckState] = useState([false, false, false, false, false, false, false]);
+
+  const days = ["Monday", "Thursday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+  console.log(date);
+
+  function add(day: any, sub: any) {
+    if (checkState[day]) {
+      if (date[day][sub].status == "add") {
+        date[day][sub].status = "delete"
+        date[day].push({ from: 0, to: 95, status: "add" });
+        if (date[day].length == 0) {
+          checkState[day] = false;
+          setCheckState([...checkState]);
+        }
+      } else {
+        date[day].splice(sub, 1);
+      }
+      setInputFields([...date]);
     }
 
+  }
+
+  function onChangeCheck(day: number) {
+    checkState[day] = !checkState[day];
+    setCheckState([...checkState]);
+
+    if (!checkState[day]) {
+      date[day] = [{ from: 0, to: 95, status: "add" }];
+      setInputFields([...date]);
+    }
+  }
+
+  function onChangeHour(index: any, sub: any, value: any) {
+    date[index][sub] = value;
+    setInputFields([...date]);
+  }
 
   return (
-    <AppLayout>
+    <AppLayout title="title">
 
       <ScreenerStage />
       <div className="py-12">
@@ -71,103 +101,132 @@ export default function Dashboard() {
 
           <div className="bg-white overflow-hidden shadow-xl sm:rounded-lg p-20">
 
-            <div className="grid grid-cols-3 py-2">
+            {
+              days.map((day, i) =>
+                <div className="grid grid-cols-3 py-2">
 
-                <div className="col-span-1">
-                  <Checkbox checked />
-                  <span className="pl-4">Monday</span>
-                </div>
+                  <div className="col-span-1">
+                    <Checkbox onChange={() => onChangeCheck(i)} checked={checkState[i]} />
+                    <span className="pl-4">{day}</span>
+                  </div>
 
-                <div className="col-span-2 flex gap-4 items-center">
-                    {date.monday.map(date => (
-                        <SelectHours></SelectHours>
-                    ))}
-                    <div onClick={() => add('monday')} className="text-2xl main-t-c cursor-pointer">+</div>
+                  <div className="col-span-2 gap-4 items-center">
+                    {checkState[i] == false ?
+                      <div className="pl-2">Closed</div>
+                      : date[i].map((data, j) =>
+                        <div className='flex gap-4 p-1'>
+                          <SelectHours change={onChangeHour} index={i} sub={j} term={data} />
+                          <div onClick={() => add(i, j)} className="text-2xl main-t-c cursor-pointer">{date[i][j].status == "add" ? "+" : <span style={{ color: "red" }}>×</span>}</div>
+                        </div>
+                      )
+                    }
+                    {/* {date.monday.map(date => (
+                      <SelectHours></SelectHours>
+                    ))} */}
+
+                  </div>
+
                 </div>
+              )
+            }
+            {/* <div className="grid grid-cols-3 py-2">
+
+              <div className="col-span-1">
+                <Checkbox checked />
+                <span className="pl-4">Monday</span>
+              </div>
+
+              <div className="col-span-2 flex gap-4 items-center">
+                <div className="pl-2">Closed</div>
+                {date.monday.map(date => (
+                  <SelectHours></SelectHours>
+                ))}
+                <div onClick={() => add('monday')} className="text-2xl main-t-c cursor-pointer">+</div>
+              </div>
 
             </div>
 
             <div className="grid grid-cols-3 py-2">
 
-                <div className="col-span-1">
-                  <Checkbox />
-                  <span className="pl-4">Thursday</span>
-                </div>
+              <div className="col-span-1">
+                <Checkbox />
+                <span className="pl-4">Thursday</span>
+              </div>
 
-                <div className="col-span-2 flex gap-4 items-center">
-                    <div className="pl-2">Closed</div>
-                </div>
-
-            </div>
-
-            <div className="grid grid-cols-3 py-2">
-
-                <div className="col-span-1">
-                  <Checkbox />
-                  <span className="pl-4">Wednesday</span>
-                </div>
-
-                <div className="col-span-2 flex gap-4 items-center">
-                    <div className="pl-2">Closed</div>
-                </div>
+              <div className="col-span-2 flex gap-4 items-center">
+                <div className="pl-2">Closed</div>
+              </div>
 
             </div>
 
             <div className="grid grid-cols-3 py-2">
 
-                <div className="col-span-1">
-                  <Checkbox />
-                  <span className="pl-4">Thursday</span>
-                </div>
+              <div className="col-span-1">
+                <Checkbox />
+                <span className="pl-4">Wednesday</span>
+              </div>
 
-                <div className="col-span-2 flex gap-4 items-center">
-                    <div className="pl-2">Closed</div>
-                </div>
-
-            </div>
-
-            <div className="grid grid-cols-3 py-2">
-
-                <div className="col-span-1">
-                  <Checkbox />
-                  <span className="pl-4">Friday</span>
-                </div>
-
-                <div className="col-span-2 flex gap-4 items-center">
-                    <div className="pl-2">Closed</div>
-                </div>
+              <div className="col-span-2 flex gap-4 items-center">
+                <div className="pl-2">Closed</div>
+              </div>
 
             </div>
 
             <div className="grid grid-cols-3 py-2">
 
-                <div className="col-span-1">
-                  <Checkbox />
-                  <span className="pl-4">Saturday</span>
-                </div>
+              <div className="col-span-1">
+                <Checkbox />
+                <span className="pl-4">Thursday</span>
+              </div>
 
-                <div className="col-span-2 flex gap-4 items-center">
-                    <div className="pl-2">Closed</div>
-                </div>
+              <div className="col-span-2 flex gap-4 items-center">
+                <div className="pl-2">Closed</div>
+              </div>
 
             </div>
 
             <div className="grid grid-cols-3 py-2">
 
-                <div className="col-span-1">
-                  <Checkbox />
-                  <span className="pl-4">Sunday</span>
-                </div>
+              <div className="col-span-1">
+                <Checkbox />
+                <span className="pl-4">Friday</span>
+              </div>
 
-                <div className="col-span-2 flex gap-4 items-center">
-                    <div className="pl-2">Closed</div>
-                </div>
+              <div className="col-span-2 flex gap-4 items-center">
+                <div className="pl-2">Closed</div>
+              </div>
 
             </div>
+
+            <div className="grid grid-cols-3 py-2">
+
+              <div className="col-span-1">
+                <Checkbox />
+                <span className="pl-4">Saturday</span>
+              </div>
+
+              <div className="col-span-2 flex gap-4 items-center">
+                <div className="pl-2">Closed</div>
+              </div>
+
+            </div>
+
+            <div className="grid grid-cols-3 py-2">
+
+              <div className="col-span-1">
+                <Checkbox />
+                <span className="pl-4">Sunday</span>
+              </div>
+
+              <div className="col-span-2 flex gap-4 items-center">
+                <div className="pl-2">Closed</div>
+              </div>
+
+            </div> */}
 
             <div>
-            <InertiaLink href={'/user/business/workplace'}>
-              <button className="main-bg-c text-white mt-4 w-full text-center rounded p-2.5">Continue</button>
+              <InertiaLink href={'/user/business/workplace'}>
+                <button className="main-bg-c text-white mt-4 w-full text-center rounded p-2.5">Continue</button>
               </InertiaLink>
             </div>
 
