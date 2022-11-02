0590a6e72eeeae3f996a22c67d13961491abfde2
brandonhuynh <liltroll123@gmail.com>
2022-11-01 23:12:42 -0700

    appointment

---
+++ database/factories/AppointmentFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Appointment>
+ */
+class AppointmentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'store_id' => $this->faker->randomDigit(),
+            'staff_id' => $this->faker->randomElement([1, 2, 3, 4]),
+            'customer_id' => $this->faker->randomDigit(),
+            'name' => $this->faker->randomElement(['Thom','Jane','Winston','Conor']),
+            'service' => $this->faker->randomElement(['Haircut','Trim','Lash','Wash']),
+            'date' => $this->faker->dateTimeBetween('+1 week', '+8 week'),
+            'duration' => $this->faker->time(),
+        ];
+    }
+}
---
+++ database/migrations/2022_11_02_060954_create_appointments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->id();
+            $table->integer('store_id');
+            $table->integer('staff_id')->nullable();
+            $table->integer('customer_id');
+            $table->string('name');
+            $table->string('service');
+            $table->datetime('date');
+            $table->time('duration');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('appointments');
+    }
+};
